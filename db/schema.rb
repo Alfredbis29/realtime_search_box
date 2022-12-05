512c4358f27e27d0c4c075bb069be6cec8a6b0c4
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2022-12-05 09:44:51 +0200

    generate tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_04_232511) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_065217) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "searches", force: :cascade do |t|
-    t.string "search_term"
+  create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.index ["user_id"], name: "index_searches_on_user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,5 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_04_232511) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "searches", "users"
+  add_foreign_key "articles", "users"
 end
