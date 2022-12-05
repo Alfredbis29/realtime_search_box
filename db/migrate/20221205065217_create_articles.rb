c0e18777208cd6dabc82a1b173164751e1373931
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2022-12-05 12:00:30 +0200

    create article

--- db/migrate/20221205065217_create_articles.rb
+++ db/migrate/20221205065217_create_articles.rb
@@ -1,7 +1,8 @@
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
-    
+    t.string :text
+
       t.timestamps
     end
     add_reference :articles, :user, foreign_key: {to_table: :users}
