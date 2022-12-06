43bb623d104bdcb06fbbe8fd02c42e8212695cd1
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2022-12-06 06:45:59 +0200

    testing user authentification

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
-
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(email: 'g@gmail.com', password: '123456') }
+
+  before { subject.save }
+
+  it 'email should not be empty' do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'article must be greater than or equal to zero.' do
+    subject.article_counter = -1
+    expect(subject).to_not be_valid
+  end
 end
