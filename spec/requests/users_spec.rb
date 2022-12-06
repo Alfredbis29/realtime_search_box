450968a0b7ba84b64573a20bff640689c583473f
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2022-12-06 07:10:17 +0200

    user request

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,13 @@
-require 'rails_helper'
-
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
     it "works! (now write some real specs)" do
       get users_path
       expect(response).to have_http_status(200)
     end
+
+    it 'renders the index template' do
+      expect(response.status).to render_template('index')
+      expect(response.body).to render_template('index')
   end
+ end
 end
