539a0a7713f0c3662e4a2ab2eb3326fcd297d6f1
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2022-12-06 12:18:14 +0200

    perform some test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,22 +1,16 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  subject { Post.new(title: "go", description: "how to cook chiken") }
-
+  subject { Article.new(title: 'go') }
   before { subject.save }
 
-  it "name should not be empty" do
+  it 'Should have correct name' do
+    title = 'go'
+    expect(title).to eq(subject.title)
+  end
+
+  it 'Should have the name present' do
     subject.title = nil
-    expect(subject).to_not be_valid
-  end
-
-  it "Title must not exceed 250 characters" do
-    subject.title = [1..300]
-    expect(subject).to_not be_valid
-  end
-
-  it "Description should not be empty." do
-    subject.Description =  nil
     expect(subject).to_not be_valid
   end
 end
